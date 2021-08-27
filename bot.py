be6bfdf012e5f3a4d473dc0daa5cb8cf04903e3b
odbawler <bawwla@LAPTOP-D7GPDLCT.localdomain>
2021-08-27 18:20:43 +0100

    add recipes and function

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 ################################################################################
 import os
 import random
+import yaml
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -39,10 +40,12 @@ async def on_error(event, *args, **kwargs):
 
 async def lookup_meal(foods):
     # TODO: Move this outside of code
-    meals = {'Funghi Crema': ['pasta', 'leek', 'mushroom', 'garlic', 'stock', 'cashew', 'cream',
+    meals = {"Funghi Crema": ['pasta', 'leek', 'mushroom', 'garlic', 'stock', 'cashew', 'cream',
                 'tarragon'],
-        'Spag Bol': ['spaghetti', 'carrot', 'mince', 'tomato', 'tomato puree', 'mushroom',
-            'pepper', 'onion', 'garlic', 'cheddar', 'cheese', 'stock']}
+            "Spag Bol": ['spaghetti', 'carrot', 'mince', 'tomato', 'tomato puree', 'mushroom',
+                'pepper', 'onion', 'garlic', 'cheddar', 'cheese', 'stock'],
+            "Greens Mac 'n' Cheese": ['leek', 'garlic', 'brocolli', 'butter', 'flour', 'milk', 
+                'macaroni', 'parmesan', 'cheddar', 'cheese', 'spinach', 'almonds']}
     # Construct inital list of meals from dictionary keys
     matches = list(meals.keys())
 
@@ -56,7 +59,21 @@ async def lookup_meal(foods):
     #matches = str(list(dict.fromkeys(matches)))[1:-1]
     return str(matches)[1:-1]
 
-@bot.command(name='scran', pass_context=True, help='Responds with random recipe from the book')
+async def lookup_recipe(recipe):
+    # for key, value in yaml.load(open('recipes.yaml'))['recipes'].iteritems():
+    #     print key, value
+    # with open("recipes.yaml", 'r') as stream:
+    #     content = yaml.load(stream)
+    #     for k,v in content.items():
+    #         if recipe in k:
+    #             return str(v)[1:-1]
+
+    for k, v in yaml.load(open('recipes.yaml'), yaml.FullLoader)["recipes"]:
+        if recipe in k:
+            print(str(v["Ingredients"])[1:-1])
+            return v["Ingredients"], v["Method"]
+
+@bot.command(name='scran', pass_context=True, help='Responds with matching meals ideas based on supplied ingredients')
 async def scran(ctx, *, message):
 
     # Split message by comman and strip whitespace
@@ -66,6 +83,17 @@ async def scran(ctx, *, message):
     response = matched_scran
     if not response:
         response = "There ain't no scran :("
+    await ctx.send(response)
+
+@bot.command(name='recipe', pass_context=True, help='Responds with the recipe for the selected meal')
+async def recipe(ctx, *, message):
+
+    print(message.strip())
+    recipe = await lookup_recipe(message.strip())
+
+    response = "```" + recipe["Ingredients"] + recipe["Method"] + "```"
+    if not response:
+        response = "We don't do that here >:|"
     await ctx.send(response)
 
 bot.run(TOKEN)
